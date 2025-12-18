f1642dbe78b0bd0944de632b80e45dfd8a5c9d05
PingozZ <davidecaramiello91@gmail.com>
2025-12-18 17:03:26 +0100

    Primo commit

---
+++ .angular/cache/21.0.3/Progetto/vite/deps/@angular_cdk_scrolling.js
@@ -0,0 +1,48 @@
+import {
+  CdkFixedSizeVirtualScroll,
+  CdkScrollable,
+  CdkScrollableModule,
+  CdkVirtualForOf,
+  CdkVirtualScrollViewport,
+  CdkVirtualScrollable,
+  CdkVirtualScrollableElement,
+  CdkVirtualScrollableWindow,
+  DEFAULT_RESIZE_TIME,
+  DEFAULT_SCROLL_TIME,
+  FixedSizeVirtualScrollStrategy,
+  ScrollDispatcher,
+  ScrollingModule,
+  VIRTUAL_SCROLLABLE,
+  VIRTUAL_SCROLL_STRATEGY,
+  ViewportRuler,
+  _fixedSizeVirtualScrollStrategyFactory
+} from "./chunk-JCDJ24PS.js";
+import "./chunk-SYEQ5TTT.js";
+import "./chunk-LPQDFLGW.js";
+import "./chunk-TXNPYDXS.js";
+import "./chunk-SXE4YVY7.js";
+import {
+  Dir
+} from "./chunk-KLBVJCC4.js";
+import "./chunk-GZE5PBKX.js";
+import "./chunk-R327OCYJ.js";
+export {
+  CdkFixedSizeVirtualScroll,
+  CdkScrollable,
+  CdkScrollableModule,
+  CdkVirtualForOf,
+  CdkVirtualScrollViewport,
+  CdkVirtualScrollable,
+  CdkVirtualScrollableElement,
+  CdkVirtualScrollableWindow,
+  DEFAULT_RESIZE_TIME,
+  DEFAULT_SCROLL_TIME,
+  FixedSizeVirtualScrollStrategy,
+  ScrollDispatcher,
+  ScrollingModule,
+  VIRTUAL_SCROLLABLE,
+  VIRTUAL_SCROLL_STRATEGY,
+  ViewportRuler,
+  _fixedSizeVirtualScrollStrategyFactory,
+  Dir as ɵɵDir
+};
